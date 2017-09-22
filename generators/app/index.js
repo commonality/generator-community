12fba1186a1cffd6f8cabab144eaf17bc819fde0
Greg Swindle <greg@swindle.net>
2017-09-22 00:54:51 -0500

    feat(product-manifest): extend generator-node:app (#4,#6)
    * feat(product-manifest): extend generator-node:app
    
    Generate:
    * .editorconfig
    * .gitattributes
    * .gitignore
    * LICENSE
    * package-lock.json
    * package.json
    
    Initialize a (local) Git repository, too, if one doesn't exist.
    
    #4
    
    * ci(windows,sonar): build on windows, check quality on sonarcloud
    
    #4,#6
    
    * ci(windows,sonar): build on windows, check quality on sonarcloud

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,52 +1,137 @@
-'use strict';
-const Generator = require('yeoman-generator');
-const chalk = require('chalk');
-const yosay = require('yosay');
+'use strict'
 
-module.exports = class extends Generator {
-  // Constructor(args, options) {
-  //   super(args, options)
-  //
-  //   this.option('readme', {
-  //     type: String,
-  //     required: false,
-  //     desc: 'Content to insert in the README.md file'
-  //   })
-  // }
+const NodeAppGenerator = require('generator-node/generators/app')
+const _ = require('lodash')
+const inquirerNpmName = require('inquirer-npm-name')
+const path = require('path')
 
-  prompting() {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the fantastic ' + chalk.red('generator-community') + ' generator!'
-    ));
+class CommunityAppGenerator extends NodeAppGenerator {
+  constructor (args, options) {
+    // Do not include generator-node's boilerplate, coveralls, or travis installations, and set the project root to the current directory.
+    const opts = _.merge(options, {
+      boilerplate: false,
+      coveralls: false,
+      projectRoot: './',
+      travis: false
+    })
 
-    const prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
-    }];
-
-    return this.prompt(prompts).then(props => {
-      // To access props later use this.props.someAnswer;
-      this.props = props;
-    });
+    super(args, opts)
   }
 
-  writing() {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+  initializing () {
+    this.pkg = this.fs.readJSON(this.destinationPath('package.json'), {})
+    super.initializing()
   }
 
-  default() {
+  _askForModuleName () {
+    if (this.pkg.name || this.options.name) {
+      this.props.name = this.pkg.name || _.kebabCase(this.options.name)
+      return Promise.resolve()
+    }
+
+    /* istanbul ignore next */
+    return inquirerNpmName({
+      name: 'name',
+      message: 'Product Name',
+      default: path.basename(process.cwd()),
+      filter: _.kebabCase,
+      validate (str) {
+        return str.length > 0
+      }
+    }, this).then((answer) => {
+      this.props.name = answer.name
+    })
+  }
+
+  prompting () {
+    return super.prompting()
+  }
+
+  default () {
+    if (this.options.editorconfig) {
+      this.composeWith(
+        require.resolve('generator-node/generators/editorconfig')
+      )
+    }
+
     this.composeWith(
-      require.resolve('generator-standard-readme/app')
-    );
+      require.resolve('generator-node/generators/nsp')
+    )
+
+    this.composeWith(require.resolve('generator-node/generators/git'), {
+      name: this.props.name,
+      githubAccount: this.props.githubAccount
+    })
+
+    if (this.options.cli) {
+      this.composeWith(
+        require.resolve('generator-node/generators/cli')
+      )
+    }
+
+    if (this.options.license && !this.pkg.license) {
+      this.composeWith(require.resolve('generator-license/app'), {
+        name: this.props.authorName,
+        email: this.props.authorEmail,
+        website: this.props.authorUrl
+      })
+    }
+
+    // If (!this.fs.exists(this.destinationPath('README.md'))) {
+    //   this.composeWith(require(generatorNode.readme), {
+    //     name: this.props.name,
+    //     description: this.props.description,
+    //     githubAccount: this.props.githubAccount,
+    //     authorName: this.props.authorName,
+    //     authorUrl: this.props.authorUrl,
+    //     coveralls: this.props.includeCoveralls,
+    //     content: this.options.readme
+    //   })
+    // }
   }
 
-  install() {
-    this.installDependencies();
+  writing () {
+    // Re-read the content at this point because a composed generator might modify it.
+    const currentPkg = this.fs.readJSON(this.destinationPath('package.json'), {})
+
+    const pkgTemplate = require('./templates/package.json')
+
+    const issuesUrl = `https://github.com/${this.props.githubAccount}/${this.props.name}`
+
+    const pkg = _.merge({
+      bugs: issuesUrl,
+      name: _.kebabCase(this.props.name),
+      version: '0.0.0',
+      description: this.props.description,
+      homepage: this.props.homepage,
+      author: {
+        name: this.props.authorName,
+        email: this.props.authorEmail,
+        url: this.props.authorUrl
+      },
+      keywords: []
+    }, pkgTemplate, currentPkg)
+
+    // Combine the keywords
+    if (this.props.keywords && this.props.keywords.length) {
+      pkg.keywords = _.uniq(this.props.keywords.concat(pkg.keywords))
+    }
+
+    // Let's extend package.json so we're not overwriting user previous fields
+    this.fs.writeJSON(this.destinationPath('package.json'), pkg)
   }
-};
+
+  install () {
+    super.installDependencies({
+      npm: true,
+      bower: false,
+      yarn: false
+    })
+  }
+
+  end () {
+    this.log('Thank you for generating community!')
+  }
+}
+
+module.exports = CommunityAppGenerator
